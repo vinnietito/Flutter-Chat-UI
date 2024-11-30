2d170e600a752cbba771a2730b82d2ee6ed6cd8f
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 18:35:09 +0300

    Commits

--- chatapp2/lib/widgets/favorite_contacts.dart
+++ chatapp2/lib/widgets/favorite_contacts.dart
@@ -5,19 +5,24 @@ const FavoriteContacts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
                   const Text(
                     'Favourites Contacts', 
                   style: TextStyle(
                     color: Colors.blueGrey, 
                     fontSize: 18.0, 
                     fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
                   ),
                 ),
                 IconButton(
                     icon: const Icon(
                       Icons.more_horiz,
                       ),
+                      iconSize: 30.0,
+                      color: Colors.blueAccent,
                       onPressed: () {},
                   ),
                 ],
