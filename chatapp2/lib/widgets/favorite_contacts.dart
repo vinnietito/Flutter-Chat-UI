3134535d3dfecc5276f1eabac568f7fbcb85c075
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 18:54:10 +0300

    Commits

--- chatapp2/lib/widgets/favorite_contacts.dart
+++ chatapp2/lib/widgets/favorite_contacts.dart
@@ -37,9 +37,11 @@ const FavoriteContacts({super.key});
           height: 120.0,
           color: Colors.blue,
           child: ListView.builder(
+            padding: EdgeInsets.only(left: 10.0),
+            scrollDirection: Axis.horizontal,
             itemCount: favorites.length,
             itemBuilder: (BuildContext context, int index) {
-              return ;
+              return Text(favorites[index].name);
             }
             
             ),
