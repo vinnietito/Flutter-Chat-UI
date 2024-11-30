f12c19aad875f246e79e1f09566f4bfbfc8f2b5c
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 10:51:35 +0300

    Commits

--- chatapp2/lib/screens/home_screen.dart
+++ chatapp2/lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _HomeScreenState createState() => _HomeScreenState();
 }
 
@@ -9,6 +12,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Color.white,
+          onPressed: () {},
+          ),
+        title: const Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
 
     );
   }
