8fe993c8af67dfd4d8b3a778a2790e35463b8a4b
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 10:42:35 +0300

    Commits

--- chatapp2/lib/main.dart
+++ chatapp2/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:chatapp2/screens/home_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Chat UI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.red,
+        hintColor: const Color(0xFFFEF9EB),
       ),
-      home: HomeScreen(),
+      home: HomeScreen(), // Removed const
     );
   }
 }
