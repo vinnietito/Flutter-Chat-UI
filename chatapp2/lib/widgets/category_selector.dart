9423bf6e3ba4d807310e0caab4734ce945364154
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 16:00:49 +0300

    Commits

--- chatapp2/lib/widgets/category_selector.dart
+++ chatapp2/lib/widgets/category_selector.dart
@@ -8,40 +8,39 @@ class CategorySelector extends StatefulWidget {
 }
 
 class _CategorySelectorState extends State<CategorySelector> {
-
   int selectedIndex = 0;
   final List<String> categories = ['Messages', 'Online', 'Groups', 'Requests'];
+
   @override
   Widget build(BuildContext context) {
-    return Container(height: 90.0,
-        color: Colors.blue,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-              child: Padding(
-
-                padding: const EdgeInsets.symmetric(
-                horizontal: 20.0, 
+    return Container(
+      height: 90.0,
+      color: Theme.of(context).primaryColor, // Use the theme's primary color
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
                 vertical: 30.0,
               ),
               child: Text(
-                categories[index], 
+                categories[index],
                 style: TextStyle(
-                color: index == selectedIndex ? Colors.white : Colors.white60,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.2,
+                  color: index == selectedIndex ? Colors.white : Colors.white60,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                ),
               ),
             ),
-              
-              ),
           );
         },
       ),
