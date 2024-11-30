bb5ce2c2f8faab8b726b561684557eeb469d5ed1
vinnietito <vincentnjuguna69@gmail.com>
2024-11-30 11:56:04 +0300

    Commits

--- chatapp2/lib/widgets/category_selector.dart
+++ chatapp2/lib/widgets/category_selector.dart
@@ -8,10 +8,22 @@ class CategorySelector extends StatefulWidget {
 }
 
 class _CategorySelectorState extends State<CategorySelector> {
+
+  int selectedIndex = 0;
+  final List<String> categories = ['Messages', 'Online', 'Groups', 'Requests'];
   @override
   Widget build(BuildContext context) {
     return Container(height: 90.0,
         color: Colors.blue,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (BuildContext context, int index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0,),
+            );
+          },
+        ),
         );
   }
 }
