103487ed140fed6448e7452174b7826ac4f02c2a
vinnietito <vincentnjuguna69@gmail.com>
2024-12-06 08:40:39 +0300

    Commits

--- chatapp2/lib/widgets/favorite_contacts.dart
+++ chatapp2/lib/widgets/favorite_contacts.dart
@@ -6,64 +6,67 @@ const FavoriteContacts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-                        const Text(
-                          'Favourites Contacts', 
-                        style: TextStyle(
-                          color: Colors.blueGrey, 
-                          fontSize: 18.0, 
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.0,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                          const Text(
+                            'Favourites Contacts', 
+                          style: TextStyle(
+                            color: Colors.blueGrey, 
+                            fontSize: 18.0, 
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1.0,
+                          ),
+                        ),
+                        IconButton(
+                            icon: const Icon(
+                              Icons.more_horiz,
+                              ),
+                              iconSize: 30.0,
+                              color: Colors.blueAccent,
+                              onPressed: () {},
+                          ),
+                        ],
+                      ),
+          ),
+          Container(
+            height: 120.0,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(left: 10.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: favorites.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(favorites[index].imageUrl),
+                        ),
+                        const SizedBox(height: 6.0),
+                      Text(favorites[index].name, 
+                      style: const TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
                         ),
                       ),
-                      IconButton(
-                          icon: const Icon(
-                            Icons.more_horiz,
-                            ),
-                            iconSize: 30.0,
-                            color: Colors.blueAccent,
-                            onPressed: () {},
-                        ),
-                      ],
-                    ),
-        ),
-        Container(
-          height: 120.0,
-          child: ListView.builder(
-            padding: const EdgeInsets.only(left: 10.0),
-            scrollDirection: Axis.horizontal,
-            itemCount: favorites.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 35.0,
-                      backgroundImage: AssetImage(favorites[index].imageUrl),
-                      ),
-                      const SizedBox(height: 6.0),
-                    Text(favorites[index].name, 
-                    style: const TextStyle(
-                      color: Colors.blueGrey,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            }
-            
-            ),
-        )
-      ],
+                    ],
+                  ),
+                );
+              }
+              
+              ),
+          )
+        ],
+      ),
     );
   }
 }
